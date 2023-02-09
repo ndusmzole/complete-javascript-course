a478d70ec15a4642c6f71b48681411801c0d0246
ndusmzole <nduduzosmsomi@gmail.com>
2023-02-09 23:31:33 +0200

    developer skills

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,23 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+//coding challenge #1
+
+const printForecast = function(temperature){
+    let forecastSentence = "";
+
+    if(!temperature.length){
+        return "The forecast array is empty";
+    }
+
+   
+    for(let i=0;i<temperature.length;i++){
+        forecastSentence += `${temperature[i]}ºC in ${i+1} days ... `;
+        
+    }
+
+    return forecastSentence;
+
+}
+
+console.log(printForecast([12, 5, -5, 0, 4]));
